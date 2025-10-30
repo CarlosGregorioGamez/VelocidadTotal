33ddbce565776aa64c218501cab0540f3ff7a73a
Carlos Gregorio <Carlosgregorio3@gmail.com>
2025-10-30 18:42:33 +0100

    Primer ejemplo de Card

---
+++ AppF1/app/src/main/java/com/example/appf1/components/CardComponent.kt
@@ -0,0 +1,62 @@
+package com.example.appf1.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.appf1.R
+
+
+class CardDetails(var imgId: Int, var imgDesc: String = "", var title: String)
+
+
+@Composable
+fun CardComponent(CardInfo: CardDetails) {
+    Card(
+        modifier = Modifier
+            .width(width = 250.dp)
+            .height(height = 125.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxSize(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = CardInfo.imgId),
+                contentDescription = CardInfo.imgDesc,
+                modifier = Modifier.padding(6.dp)
+            )
+            TitleComponent(
+                title = "Ejemplo de titulo",
+                textStyle = MaterialTheme.typography.titleSmall,
+                textColor = MaterialTheme.colorScheme.secondary
+            )
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun CardComponentPreview() {
+    CardComponent(
+        CardInfo = CardDetails(
+            imgId = R.drawable.ic_launcher_foreground,
+            imgDesc = "Descripcion",
+            title = "Titulo de ejemplo"
+        )
+    )
+}
