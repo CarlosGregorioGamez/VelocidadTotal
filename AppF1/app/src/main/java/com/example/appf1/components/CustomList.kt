7fd64762f30107fabc228d3d24ef3bf272f3f228
Adrián Rodríguez Roqueiro <a18adrianrr@teis.intranet>
2025-11-04 16:12:15 +0100

    Clases listas

--- AppF1/app/src/main/java/com/example/appf1/components/CustomList.kt
+++ AppF1/app/src/main/java/com/example/appf1/components/CustomList.kt
@@ -4,7 +4,20 @@ import androidx.compose.runtime.Composable
 
 class Piloto(var name: String, var team: String)
 
+class Carrera(var name: String, var ediciones: Int)
+
+class Equipo(var name: String, var piloto1: Piloto, var  piloto2: Piloto)
+
 @Composable
 fun CustomList(pilotos: List<Piloto>){
 
 }
+
+fun CustomListB(carrera: List<Carrera>){
+
+}
+
+fun CustomListC(equipo: List<Equipo>){
+
+}
+
