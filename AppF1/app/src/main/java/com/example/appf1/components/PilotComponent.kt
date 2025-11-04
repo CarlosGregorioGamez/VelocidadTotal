3cd21ed9768ab29459cef18830ecce30d026038b
Adrián Rodríguez Roqueiro <a18adrianrr@teis.intranet>
2025-11-04 16:20:05 +0100

    Merge

--- AppF1/app/src/main/java/com/example/appf1/components/PilotComponent.kt
+++ AppF1/app/src/main/java/com/example/appf1/components/PilotComponent.kt
@@ -14,7 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
-
+//merge
 @Composable
 fun pilot(name: String, team: String) {
 
