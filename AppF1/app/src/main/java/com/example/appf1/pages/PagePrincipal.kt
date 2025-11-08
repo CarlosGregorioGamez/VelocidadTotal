d9eb46cf5017687b542ae5045b1a28169d7f0d94
Carlos Gregorio <carlosgregorio3@gmail.com>
2025-11-08 12:37:23 +0100

    Terminamos menú principal y arreglado problema de version en casa (abierto a modificaciones)

--- AppF1/app/src/main/java/com/example/appf1/pages/PagePrincipal.kt
+++ AppF1/app/src/main/java/com/example/appf1/pages/PagePrincipal.kt
@@ -1,8 +1,10 @@
 package com.example.appf1.pages
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -12,27 +14,35 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.appf1.R
 import com.example.appf1.components.CustomButton
 import com.example.appf1.components.TitleComponent
 import com.example.compose.backgroundLight
 
 @Composable
 fun pagePrincipal() {
-    Column(modifier = Modifier,
+    Column(
+        modifier = Modifier,
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(text = "usuario", modifier = Modifier.fillMaxWidth(), backgroundLight)
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(R.drawable.ic_launcher_foreground),
+            contentDescription = "Logo principal de la App"
+        )
+        Text(text = "Usuario", modifier = Modifier, backgroundLight)
         TextField(
             "",
             onValueChange = {
 
             },
             modifier = Modifier.padding(9.dp)
-            )
-        Text(text = "contraseña", modifier = Modifier.fillMaxWidth(), backgroundLight)
+        )
+        Text(text = "Contraseña", modifier = Modifier, backgroundLight)
         TextField(
             "",
             onValueChange = {
@@ -41,14 +51,19 @@ fun pagePrincipal() {
             modifier = Modifier.padding(9.dp),
             visualTransformation = PasswordVisualTransformation()
         )
-        Row {
-            CustomButton("iniciar") { }
-            CustomButton("Salir") { }
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            CustomButton("Iniciar") {
+
+            }
+            CustomButton("Salir") {
+
+            }
         }
-
     }
-
-
 }
 
 @Preview
