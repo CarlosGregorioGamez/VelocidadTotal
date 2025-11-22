adc990a5a5cedc12621d25f16f0868e366b23606
Adrian <adrian.rodriguez.roqueiro@gmail.com>
2025-11-22 11:39:18 +0100

    mejoras en la pageCarreras

--- AppF1/app/src/main/java/com/example/appf1/pages/PageCarreras.kt
+++ AppF1/app/src/main/java/com/example/appf1/pages/PageCarreras.kt
@@ -4,8 +4,13 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -20,6 +25,7 @@ import com.example.appf1.components.CustomButton
 import com.example.compose.backgroundLight
 import com.example.compose.onPrimaryLight
 import com.example.compose.primaryLight
+import com.example.compose.surfaceContainerLight
 
 /**
  * Página para ver los datos en detalle de una carrera en específico
@@ -32,22 +38,40 @@ import com.example.compose.primaryLight
 @Composable
 fun pageCarrearas(nombreCarrera: String, ediciones: Int, curvas: Int, pais: String) {
     Column(
-        
-        modifier = Modifier,
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Image(
-            painter = painterResource(R.drawable.vegas),
-            contentDescription = "Logo principal de la App"
-        )
-        Text(text = "Carrera :$nombreCarrera", modifier = Modifier, onPrimaryLight)
-        Text(text = "Ediciones : $ediciones", modifier = Modifier, onPrimaryLight)
-        Text(text = "Numero de curvas : $curvas", modifier = Modifier, onPrimaryLight)
-        Text(text = "Pais : $pais", modifier = Modifier, onPrimaryLight)
-    }
-}
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,        // centra verticalmente
+        horizontalAlignment = Alignment.CenterHorizontally // centra horizontalmente
+    ){
+        Card(
+            modifier = Modifier
+                .padding(24.dp)
+                .height(400.dp)
+                .fillMaxWidth(),
+            shape = RoundedCornerShape(16.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = surfaceContainerLight
+            ),
+            elevation = CardDefaults.cardElevation(defaultElevation = 6.dp)
+        ){
+            Column(
 
+                modifier = Modifier,
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.vegas),
+                    contentDescription = "Logo principal de la App"
+                )
+                Text(text = "Carrera :$nombreCarrera", modifier = Modifier, onPrimaryLight)
+                Text(text = "Ediciones : $ediciones", modifier = Modifier, onPrimaryLight)
+                Text(text = "Numero de curvas : $curvas", modifier = Modifier, onPrimaryLight)
+                Text(text = "Pais : $pais", modifier = Modifier, onPrimaryLight)
+            }
+        }
+    }
+
+}
 @Preview
 @Composable
 fun pagePreview1() {
