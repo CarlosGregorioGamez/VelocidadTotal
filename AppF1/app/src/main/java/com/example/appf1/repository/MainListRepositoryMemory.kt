098313e66dfa423d51a376e32d369fbdf9d7a534
Adrián Rodríguez Roqueiro <a18adrianrr@teis.intranet>
2026-01-20 17:10:54 +0100

    iniciando en las listas d pilotos

--- AppF1/app/src/main/java/com/example/appf1/repository/MainListRepositoryMemory.kt
+++ AppF1/app/src/main/java/com/example/appf1/repository/MainListRepositoryMemory.kt
@@ -43,4 +43,30 @@ class MainListRepositoryMemory {
         CardSliderDetails(imgId = R.drawable.red_bull_racing, title = "Red Bull Racing F1"),
         CardSliderDetails(imgId = R.drawable.williams, title = "Williams F1"),
     )
+
+    fun getPilots(): List<CardSliderDetails> = listOf(
+        CardSliderDetails(imgId = R.drawable.albon, title = "Albon"),
+        CardSliderDetails(imgId = R.drawable.alonso, title = "Alonso"),
+        CardSliderDetails(imgId = R.drawable.antonelli, title = "Antonelli"),
+        CardSliderDetails(imgId = R.drawable.australia, title = "Gp de Australia"),
+        CardSliderDetails(imgId = R.drawable.austria, title = "GP de Austria"),
+        CardSliderDetails(imgId = R.drawable.azerbaijan, title = "GP de Azerbaijan"),
+        CardSliderDetails(imgId = R.drawable.bahrein, title = "GP de Bahrain"),
+        CardSliderDetails(imgId = R.drawable.belgica, title = "GP de Bélgica"),
+        CardSliderDetails(imgId = R.drawable.brasil, title = "GP de Brasil"),
+        CardSliderDetails(imgId = R.drawable.canada, title = "GP de Canadá"),
+        CardSliderDetails(imgId = R.drawable.china, title = "GP de China"),
+        CardSliderDetails(imgId = R.drawable.espana, title = "GP de España"),
+        CardSliderDetails(imgId = R.drawable.gran_bretana, title = "GP de Gran Bretaña"),
+        CardSliderDetails(imgId = R.drawable.hungria, title = "GP de Hungría"),
+        CardSliderDetails(imgId = R.drawable.italia, title = "GP de Italia"),
+        CardSliderDetails(imgId = R.drawable.japon, title = "GP de Japón"),
+        CardSliderDetails(imgId = R.drawable.mexico, title = "GP de México"),
+        CardSliderDetails(imgId = R.drawable.miami, title = "GP de Miami"),
+        CardSliderDetails(imgId = R.drawable.monaco, title = "GP de Mónaco"),
+        CardSliderDetails(imgId = R.drawable.paises_bajos, title = "GP de Países Bajos"),
+        CardSliderDetails(imgId = R.drawable.qatar, title = "GP de Qatar"),
+        CardSliderDetails(imgId = R.drawable.singapur, title = "GP de Singapur"),
+        CardSliderDetails(imgId = R.drawable.vegas, title = "GP de Las Vegas"),
+    )
 }
