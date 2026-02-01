e5b0cc10f7c11b4d20b422bdb7755541dada955f
Adrian <adrian.rodriguez.roqueiro@gmail.com>
2026-02-01 13:20:16 +0100

    usuarios de prueba

--- AppF1/app/src/main/java/com/example/appf1/repository/UserRepostoryMemory.kt
+++ AppF1/app/src/main/java/com/example/appf1/repository/UserRepostoryMemory.kt
@@ -6,6 +6,17 @@ class UserRepostoryMemory : UserRepository {
 
     private val users: ArrayList<UserDTO> = ArrayList()
 
+    companion object{
+        val usuarios = ArrayList<UserDTO>(
+            listOf(
+                UserDTO(0, "Estudiante 1", "Taller 10"),
+                UserDTO(1, "Estudiante 2", "Taller 10"),
+                UserDTO(2, "Estudiante 3", "Taller 10"),
+                UserDTO(3, "Estudiante 4", "Taller 10")
+            ))
+        var currId = 4
+    }
+
     constructor() {
         for (i in 1..10) {
             users.add(
