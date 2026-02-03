84be0daef1403c5d16539b71985d790fb5e3e0c7
Adrián Rodríguez Roqueiro <a18adrianrr@teis.intranet>
2026-02-03 16:16:39 +0100

    variables de la nav

---
+++ AppF1/app/src/main/java/com/example/appf1/F1NavGraph.kt
@@ -0,0 +1,12 @@
+package com.example.appf1
+
+object Routes {
+    const val MainMenu = "mainMenu"
+    const val pageCarreras = "pageCarreras/{Id}"
+    const val pageEquipos="pageEquipos/{Id}"
+    const val pageLogin= "pageLogin"
+    const val pagePerfil = "pagePerfil/{id}"
+    const val pagePilotos ="pagePilotos/{id}"
+
+
+}
