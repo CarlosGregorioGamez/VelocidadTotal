cab0af2ed50cf40b51e275a65980fc3f2e9ec0f0
Carlos Gregorio <Carlosgregorio3@gmail.com>
2026-02-11 15:09:12 +0100

    Creamos listas para la data de retrofit, están actualizados los datos en el repo de data

--- AppF1/app/src/main/java/com/example/appf1/data/model/PilotoDTO.kt
+++ AppF1/app/src/main/java/com/example/appf1/data/model/PilotoDTO.kt
@@ -3,10 +3,10 @@ package com.example.appf1.data.model
 
 data class PilotoDTO(val id: String,
                      val name: String,
-                     val team: EquipoDTO,
+                     val team: String,
                      val wins: List<String> = emptyList(),
                      val podiums: List<String> = emptyList(),
                      val poles: Int = 0,
                      val imgId: Int,
-                     val carreras: List<CarreraDTO> = emptyList()
+                     val carreras: List<String> = emptyList()
 )
