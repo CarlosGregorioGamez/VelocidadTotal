ee9b4ea4aee8dc6b25895d2d6fa280c746183ef7
Carlos Gregorio <carlosgregorio3@gmail.com>
2026-02-18 11:35:09 +0100

    Adaptamos el AppF1Service

--- AppF1/app/src/main/java/com/example/appf1/network/AppF1Service.kt
+++ AppF1/app/src/main/java/com/example/appf1/network/AppF1Service.kt
@@ -1,6 +1,5 @@
 package com.example.appf1.network
 
-import com.example.appf1.data.model.EquipoDTO
 import com.example.appf1.data.model.UserDTO
 import com.google.gson.annotations.SerializedName
 import retrofit2.Response
@@ -10,81 +9,118 @@ import retrofit2.http.POST
 import retrofit2.http.Path
 import retrofit2.http.Query
 
-interface AppF1Service{
+interface AppF1Service {
 
     @POST("appF1/login")
     suspend fun login(
         @Body loginRequest: LoginDTO
     ): Response<LoginResponseDTO>
 
-@GET("piloto/{id}")
-suspend fun getPiloto(@Path("id") id: String): Response<PilotoResponse>
+    @GET("piloto/{id}")
+    suspend fun getPiloto(@Path("id") id: String): Response<PilotoResponse>
+
+    @GET("piloto")
+    suspend fun getPilotoList(
+        @Query("limit") limit: Int = 50,
+        @Query("offset") offset: Int = 0
+    ): Response<PilotoListResponse>
+}
+
+@GET("carrera/{id}")
+suspend fun getCarrera(@Path("id") id: String): Response<CarreraResponse>
 
 @GET("piloto")
-suspend fun getPilotoList(@Query("limit") limit: Int = 50, @Query("offset") offset: Int = 0): Response<PokemonListResponse>
+suspend fun getCarreraList(
+    @Query("limit") limit: Int = 50,
+    @Query("offset") offset: Int = 0
+): Response<CarreraListResponse>
+}
+
+@GET("piloto/{id}")
+suspend fun getEquipo(@Path("id") id: String): Response<EquipoResponse>
+
+@GET("piloto")
+suspend fun getEquipoList(
+    @Query("limit") limit: Int = 50,
+    @Query("offset") offset: Int = 0
+): Response<EquipoListResponse>
 }
 
 data class PilotoResponse(
     val id: String,
     val name: String,
-    @SerializedName("sprites") val sprites: Sprites,
-    val team: EquipoDTO,
-    @SerializedName("victorias") val wins: List<StatEntry> = emptyList(),
-    @SerializedName("podios") val podios: List<MoveEntry> = emptyList()
+    val team: String,
+    @SerializedName("victorias") val wins: List<WinsEntry> = emptyList(),
+    @SerializedName("podios") val podios: List<PodiumEntry> = emptyList(),
+    val poles: Int = 0,
+    val imgId: Int,
 )
+
+data class CarreraResponse(
+    val id: String,
+    val name: String,
+    val editions: Int = 0,
+    val country: String = "",
+    val winner: String = "",
+    val podium: List<String> = emptyList(),
+    val length: Double = 0.0,
+    val imgId: Int
+)
+
+data class EquipoResponse(
+    val id: String,
+    val name: String,val drivers: List<String> = emptyList(),
+    val championships: Int = 0,
+    val wins: Int = 0,
+    val podiums: Int = 0,
+    val imgId: Int
+    )
 
 data class LoginDTO(
     @SerializedName("email") val email: String,
     @SerializedName("password") val password: String
 )
 
+class WinsEntry(
+    @SerializedName("wins") val wins: List<String>
+)
 
-class MovesEntry {
-    @SerializedName("name") val name: String = ""
+class PodiumEntry {
+    @SerializedName("podium")
+    val podium: List<String> = emptyList()
 }
 
-class StatEntry(
-    @SerializedName("base_stat") val baseStat: Int,
-    val stat: StatRef
-)
-
-class StatRef(
-    val name: String,
-    val url: String
-)
-
-data class Sprites(
-    @SerializedName("front_default") val frontDefault: String?
-)
 
 data class LoginResponseDTO(
     val user: UserDTO
 )
 
-data class PokemonListResponse(
-    val results: List<PokemonListItem>
+data class PilotoListResponse(
+    val results: List<PilotoListItem>
 )
 
-data class PokemonListItem(
+data class PilotoListItem(
     val name: String,
     val url: String
 )
 
-data class TypeEntry(
-    val slot: Int,
-    val type: TypeRef
+data class EquipoListResponse(
+    val results: List<EquipoListItem>
 )
 
-data class TypeRef(
+data class EquipoListItem(
     val name: String,
     val url: String
 )
 
-data class MoveEntry(
-    val move: MoveRef
+
+data class CarreraListResponse(
+    val results: List<CarreraListItem>
 )
 
-data class MoveRef(
+data class CarreraListItem(
     val name: String,
     val url: String
 )
+
+
