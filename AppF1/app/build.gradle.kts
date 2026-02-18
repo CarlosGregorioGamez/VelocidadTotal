32746ae270fa70a74c1759f6326eb030e6fc990e
Adrian <adrian.rodriguez.roqueiro@gmail.com>
2026-02-18 13:07:13 +0100

    librerias y dependencias agregadas

--- AppF1/app/build.gradle.kts
+++ AppF1/app/build.gradle.kts
@@ -66,4 +66,10 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    implementation(libs.coil.compose)
+    implementation(libs.coil.network.okhttp)
+
+    implementation(libs.retrofit2)
+    implementation(libs.retrofit2.converter.gson)
+    implementation(libs.okhttp.logging)
 }
